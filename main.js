4d52e8a0855a8881c66c0a5896dc50a0759414d1
Chris Hile <chile@student.fullsail.edu>
2021-09-24 09:48:05 -0400

    finalproject

--- main.js
+++ main.js
@@ -1,35 +1,75 @@
+let margin = document.getElementById('myButton').style.marginBottom = 50 + 'px';
 
 let quest = document.getElementById('myButton').onclick = saving;
 
-function saving(){
+function saving() {
     document.getElementById('response').innerHTML = "Would you like to save 10% and join my newsletter?";
     document.getElementById('myButton').remove();
 
-
-    let x = document.createElement("INPUT");
-    x.setAttribute("type", "text");
-    document.body.appendChild(x);
-
     let y = document.createElement('button');
-    y.innerHTML = "Submit";
-    y.type = "frmBtn";
+    y.innerHTML = "Yes";
+    y.type = 'button';
+    y.id = 'button';
+    y.className = 'center';
     document.body.appendChild(y);
-}
 
-/****
-let want = document.querySelector('button').onclick = discount;
+    let z = document.createElement('button');
+    z.innerHTML = "No";
+    z.type = 'button';
+    z.id = 'button2';
+    z.className = 'center';
+    z.style.marginBottom = 50 + "px";
+    document.body.appendChild(z);
 
-function discount() {
 
-    let response = document.querySelector('input').value;
-    if (response === "yes") {
+    let myBtn = document.getElementById('button').onclick = yes;
+    let myBtn2 = document.getElementById('button2').onclick = no;
 
-        let email = document.getElementById("response").innerHTML = "Enter a valid email address:";
-        while (email === "") {
-            email = prompt("Please enter valid email address:")
+    function no(){
+        document.getElementById('response').innerHTML = "Thanks for visiting!"
+        document.getElementById('button').remove();
+        document.getElementById('button2').remove();
+    }
+    function yes() {
+        let x = document.createElement("input");
+        x.type = 'text';
+        x.id = 'input';
+        x.className = 'center';
+        document.body.appendChild(x);
+
+        let submit = document.createElement('button');
+        submit.innerHTML = "submit";
+        submit.type = 'button';
+        submit.id = 'submit';
+        submit.className = 'center';
+        submit.style.marginBottom = 50 + "px";
+        document.body.appendChild(submit);
+
+        document.getElementById('button').remove();
+        document.getElementById('button2').remove();
+
+        document.getElementById('response').innerHTML = "Enter email address:";
+
+
+
+        document.getElementById('submit').onclick = getValue;
+
+        function getValue(){
+            let validate = document.getElementById('input').value;
+            email(validate);
         }
 
-        function disCode(length) {
+        function email(validate){
+
+            if (validate === ""){
+               document.getElementById('response').innerHTML = "Please enter email address:";
+            }
+            else {
+                discode(5);
+            }
+        }
+
+        function discode(length) {
             let result = '';
             let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
             let charactersLength = characters.length;
@@ -37,14 +77,18 @@ function discount() {
                 result += characters.charAt(Math.floor(Math.random() *
                     charactersLength));
             }
-            return result;
+            document.getElementById('response').innerHTML = "Your discound code is: " + result;
+            document.getElementById('response').style.marginBottom = 50 + 'px';
+            document.getElementById('input').remove();
+            document.getElementById('submit').remove();
         }
-
-        let discount = document.getElementById('response').innerHTML = "Welcome! Your discount code is: " + disCode(5);
-
-    } else {
-        let no = document.getElementById('response').innerHTML = "Thank you for visiting!";
     }
 
+
+
+
+
+
+
 }
-*/
+
